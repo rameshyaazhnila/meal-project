655c6db48d9bf671f374a236d8b5fdf60aabb94a
rameshyaazhnila <rameshyaazhnila@gmail.com>
2024-07-19 09:39:29 +0530

    meal project

--- meal/src/App.jsx
+++ meal/src/App.jsx
@@ -1,7 +1,14 @@
-export default function App() {
+import React from 'react'
+import image from "./assets/png.png"
+
+const App = () => {
   return (
-    <div className="container">
+    <div className="container bg-gradient-to-b from-white to-black pb-20">
       <h1 className="lg:text-blue-600 p-24 sm:p-10 md:p-0 lg:p-5">hello ramesh</h1>
+      <img className='shadow' src={image} alt="" />
+      <h2 className='text-white'>one pic is successfully created</h2>
     </div>
   )
 }
+
+export default App
